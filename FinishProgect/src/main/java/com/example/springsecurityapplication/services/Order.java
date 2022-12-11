61a0688e7a1d57b3cf018bfa9b7a2804a566d8ba
Lokteeva <119425043+Lokteeva@users.noreply.github.com>
2022-12-11 19:29:45 +0300

    first commit
    first commit

---
+++ FinishProgect/src/main/java/com/example/springsecurityapplication/services/Order.java
@@ -0,0 +1,52 @@
+//package com.example.springsecurityapplication.services;
+//
+//import com.example.springsecurityapplication.models.Order;
+//import com.example.springsecurityapplication.repositories.OrderRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//@Transactional(readOnly = true)
+//public class OrderEdit{
+//
+//    private final OrderRepository orderRepository;
+//
+//    @Autowired
+//    public OrderService(OrderRepository orderRepository) {
+//        this.orderRepository = orderRepository;
+//    }
+//
+//    public OrderEdit(OrderRepository orderRepository) {
+//        this.orderRepository = orderRepository;
+//    }
+//
+//    // Данный метод позволяет вернуть все заказы
+//    public List<Order> getAllOrder(){
+//        return orderRepository.findAll();
+//    }
+//
+//    // Данный метод позволяет вернуть заказ по id
+//    public Order getOrderId(int id){
+//        Optional<Order> optionalOrder = orderRepository.findById(id);
+//        return optionalOrder.orElse(null);
+//    }
+//
+//    // Данный метод позволяет сохранить объект заказа, который пришел с формы
+//    @Transactional
+//    public void saveOrder(Order order){
+//        orderRepository.save(order);
+//    }
+//
+//    // Данный метод позволяет обновить информацию о заказе
+//    @Transactional
+//    public void updateOrder(int id, Order order){
+//        order.setId(id);
+//        orderRepository.save(order);
+//    }
+//
+//}
+//
